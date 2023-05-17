81f50bf1dd632ecdd6f5fea00a19bd588bc22bf1
Diogo Jorge Basso <00a9d4c1-b21e-4e56-a261-9a64648e99c1@anonaddy.me>
2023-05-17 20:14:00 -0300

    chore: add first contract and librarie to deploy it
    Signed-off-by: Diogo Jorge Basso <00a9d4c1-b21e-4e56-a261-9a64648e99c1@anonaddy.me>

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [account, setAccount] = useState<string>('');
+  const [shoes, setShoes] = useState<{ id: number; name: string; price: number }[]>([]);
+  const [shoeName, setShoeName] = useState<string>('');
+  const [shoePrice, setShoePrice] = useState<number>(0);
+  const [selectedShoe, setSelectedShoe] = useState<{ id: number; name: string; price: number } | null>(null);
+  const [message, setMessage] = useState<string>('');
+
+  const { ethereum } = window as any;
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Marketplace de Sapatos</h1>
+        <div>
+          {account ? (
+            <p>Conectado com a conta: {account}</p>
+          ) : (
+            <button onClick={connectWallet}>Conectar Wallet</button>
+          )}
+        </div>
+      </header>
+      <main className="App-main">
+        <h2>Criar Sapato</h2>
+        <form onSubmit={(event) => {
+          event.preventDefault();
+          addShoe();
+        }}>
+          <label htmlFor="shoeName">Nome do Sapato:</label>
+          <input type="text" id="shoeName" value={shoeName} onChange={(event) => setShoeName(event.target.value)} required />
+          <label htmlFor="shoePrice">Preço do Sapato:</label>
+          <input type="number" id="shoePrice" value={shoePrice} onChange={(event) => setShoePrice(parseInt(event.target.value))} required />
+          <button type="submit">Adicionar Sapato</button>
+        </form>
+        <h2>Comprar Sapatos</h2>
+        <div className="shoes-container">
+          {shoes.map(shoe => (
+            <div className="shoe" key={shoe.id}>
+              <h3>{shoe.name}</h3>
+              <p>Preço: {ethers.utils.formatEther(shoe.price)} ETH</p>
+              {selectedShoe && selectedShoe.id === shoe.id ? (
+                <button onClick={() => setSelectedShoe(null)}>Cancelar</button>
+              ) : (
+                <button onClick={() => setSelectedShoe(shoe)}>Comprar</button>
+              )}
+            </div>
+          ))}
+        </div>
+        {selectedShoe && (
+          <div className="selected-shoe">
+            <h2>Confirmar Compra</h2>
+            <p>Você tem certeza que quer comprar {selectedShoe.name} por {ethers.utils.formatEther(selectedShoe.price)} ETH?</p>
+            <button onClick={buyShoe}>Sim, Comprar</button>
+            <button onClick={() => setSelectedShoe(null)}>Cancelar</button>
+          </div>
+        )}
+        {message && <p>{message}</p>}
+      </main>
+    </div>
+  );
+}
+
+export default App;
